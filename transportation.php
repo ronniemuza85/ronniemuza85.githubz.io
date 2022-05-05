f1c41f51c87a439d94ca49413e219316e3104e5c
emuza <32819383+ronniemuza85@users.noreply.github.com>
2022-05-05 12:08:52 +0200

    Add files via upload

---
+++ transportation.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/handeyi-96x47.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>Transportation</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/animate-css/css/animate.css">
+  <link rel="stylesheet" href="assets/animate-css/css/animate.compat.css">
+  <link rel="stylesheet" href="assets/animate-css/css/animate.min.css">
+  <link rel="stylesheet" href="assets/animate-css/css/animation_utility.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/dentaura-preloader/dentaura-preloader.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,700;1,400;1,700&display=swap&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,700;1,400;1,700&display=swap&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  <script src="assets/jquery/jquery.min.js"></script>
+  
+</head>
+<body>
+  
+  <section class="animateMenu6 extMenu6 menu cid-sVjKLge97h" once="menu" id="animateMenu6-5">
+
+    
+
+
+    <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+        <div class="navbar-brand">
+            <span class="navbar-logo">
+                
+                    <img src="assets/images/handeyi-96x47.png" alt="Handeyi" style="height: 2.8rem;">
+                
+            </span>
+            <span class="navbar-caption-wrap"><a class="navbar-caption text-primary display-4" href="index.html">Handeyi Harare
+                </a></span>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item"><a class="nav-link link text-success text-primary display-4" href="attractions.html">Things to do</a></li><li class="nav-item"><a class="nav-link link text-success text-primary display-4" href="page4.html" aria-expanded="false">Upcoming Events</a></li>
+                
+
+                <li class="nav-item"><a class="nav-link link text-success text-primary display-4" href="packages.html" aria-expanded="false">Culinary &amp; Night life</a></li></ul>
+            
+
+            <div class="icons-menu">
+                
+                    <span class="p-2 mbr-iconfont socicon-whatsapp socicon" style="color: rgb(78, 139, 70); fill: rgb(78, 139, 70);"></span>
+                
+                
+                    <span class="p-2 mbr-iconfont mobi-mbri-phone mobi-mbri" style="color: rgb(250, 127, 9); fill: rgb(250, 127, 9);"></span>
+                
+                
+                
+                
+                
+            </div>
+
+        </div>
+    </nav>
+</section>
+
+<section data-bs-version="5.1" class="mbr-section cid-sVk7ZLJFNY" once="Dentaura Preloader" id="preloader12-g">
+
+    
+    <!-- Dentaura Preloader -->
+        <div id="loading-preloader">
+            <div id="loading-center-preloader">
+                <div id="loading-center-absolute">
+                    <img src="assets/images/handeyi-96x47.png" class="dentaura-logo-preloader" alt="">
+                    <div class="spinner">
+                      <div class="bounce1"></div>
+                      <div class="bounce2"></div>
+                      <div class="bounce3"></div>
+                      <div class="bounce4"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <!--End of Dentaura Preloader -->
+    
+</section>
+
+<section class="mbr-section mbr-section--no-padding cid-sVjOpqe7LC mbr-fullscreen" id="witsec-html-block-6">
+
+
+
+
+
+<!-- Place your own code below -->
+<iframe src="http://localhost/handeyi/booking/" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;frameborder:0;position:absolute;top:40px;left:0px;right:0px;bottom:0px" height="100%" width="100%" scrolling="no"></iframe>   
+<!-- Place your own code above -->
+
+</section><section id="top-1" hidden><a href="https://mobirise.site"></a></section><script src="assets/popper/popper.min.js"></script>  <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>  <script src="assets/animate-css/js/oyethemes_onscroll_animation.js"></script>  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/ytplayer/index.js"></script>  <script src="assets/tether/tether.min.js"></script>  <script src="assets/dropdown/js/navbar-dropdown.js"></script>  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>  <script src="assets/theme/js/script.js"></script>  <script src="assets/dentaura-preloader/dentaura-preloader.js"></script>  
+  
+  
+</body>
+</html>
